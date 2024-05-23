9084cf7d59f9b5df7ad38d07fcb332e788be87c7
VaranasiAsha <155607344+VaranasiAsha@users.noreply.github.com>
2024-05-23 17:14:08 +0530

    Update App.js

--- App.js
+++ App.js
@@ -3,9 +3,25 @@ import ReactDom from "react-dom";
 
 var htmlRoot = document.getElementById("root");
 var rootReact = ReactDom.createRoot(htmlRoot);
-const Header=() => (<p>Header</p>);
-const Body=() => (Body);
-const Footer=() => (Footer);
+const Header=() => (
+    <div className="header">
+        <div className="logo">
+            <img src="" alt="logo-image"/>
+            <div className="nav-bar">
+                <ul>
+                    <li>HOME</li>
+                    <li>SERVICE</li>
+                    <li>CONTACT US</li>
+                    <li>CARTS</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+);
+
+const Body=() => (<p>Body</p>);
+const Footer=() => (<p>Footer</p>);
 
 const RootComponent =() => (<><Header/><Body/><Footer/></>);
 
